e1df3d36eed037a5cafee958ae0cddf7fe5e3c31
vvzvlad <vvzvlad@gmail.com>
2019-01-15 16:06:56 +0300

    update structure

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,34 +4,36 @@ module.exports = {
    themeConfig: {
       nav: [
          {
-            text: 'Основная информация', items: [
-               { text: 'Что такое GLUE', link: '/what_glue.html' },
-               { text: 'Запуск системы', link: '/start.html' },
-               { text: 'Панель управления', link: '/panel.html' },
-               { text: 'Скрипты и драйвера', link: '/scripts_and_drivers.html' },
-               { text: 'Система логов', link: '/logs.html' },
-               { text: 'Общая шина', link: '/bus.html' },
-               { text: 'Язык Lua', link: '/lua.html' }
-            ]
+            text: 'Tutorials', items: [
+               { text: 'Запуск системы', link: '/start.html' }
+            ],
          },
          {
-            text: 'Документация', items: [
+            text: 'Howto guides', items: [
+               { text: 'Запуск системы', link: '/start.html' }
+            ],
+         },
+         {
+            text: 'Explanation', items: [
+               { text: 'Что такое GLUE', link: '/what_glue.html' },
+               { text: 'Общая шина', link: '/bus.html' },
+               { text: 'Система логов', link: '/logs.html' },
+               { text: 'Скрипты и драйвера', link: '/scripts_and_drivers.html' },
+               { text: 'Внутренности системы', link: '/inside.html' },
+               { text: 'Инструменты тестирования', link: '/tests.html' }
+            ],
+         },
+         {
+            text: 'Reference', items: [
+               { text: 'История изменений', link: '/changelog.html' },
                { text: 'Панель управления', link: '/panel.html' },
                { text: 'Руководство разработчика', link: '/developers.html' },
-               { text: 'Внутренности системы', link: '/inside.html' },
-               { text: 'Инструменты тестирования', link: '/tests.html' },
-               { text: 'История изменений', link: '/changelog.html' }
-            ]
+               { text: 'Язык Lua', link: '/lua.html' },
+               {
+                  text: 'Примеры'
+               }
+            ],
          },
-         {
-            text: 'Примеры', items: [
-               { text: 'Драйвера', link: '/examples_driver.html' },
-               { text: 'Bus-event скрипты', link: '/examples_bus_event.html' },
-               { text: 'Web-event скрипты', link: '/examples_web_event.html' },
-               { text: 'Timer-event скрипты', link: '/examples_timer_event.html' },
-               { text: 'Shedule-event скрипты', link: '/examples_shedule_event.html' }
-            ]
-         }
       ],
       sidebar: "auto",
       sidebarDepth: 3,
