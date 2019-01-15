759f907490b7269c53d4472293545d22292cbad5
vvzvlad <vvzvlad@gmail.com>
2019-01-15 16:43:06 +0300

    fix config err

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,12 +6,12 @@ module.exports = {
          {
             text: 'Tutorials', items: [
                { text: 'Запуск системы', link: '/start.html' }
-            ],
+            ]
          },
          {
             text: 'Howto guides', items: [
                { text: 'Запуск системы', link: '/start.html' }
-            ],
+            ]
          },
          {
             text: 'Explanation', items: [
@@ -21,7 +21,7 @@ module.exports = {
                { text: 'Скрипты и драйвера', link: '/scripts_and_drivers.html' },
                { text: 'Внутренности системы', link: '/inside.html' },
                { text: 'Инструменты тестирования', link: '/tests.html' }
-            ],
+            ]
          },
          {
             text: 'Reference', items: [
@@ -30,9 +30,15 @@ module.exports = {
                { text: 'Руководство разработчика', link: '/developers.html' },
                { text: 'Язык Lua', link: '/lua.html' },
                {
-                  text: 'Примеры'
+                  text: 'Примеры', items: [
+                     { text: 'Драйвера', link: '/examples_driver.html' },
+                     { text: 'Bus-event скрипты', link: '/examples_bus_event.html' },
+                     { text: 'Web-event скрипты', link: '/examples_web_event.html' },
+                     { text: 'Timer-event скрипты', link: '/examples_timer_event.html' },
+                     { text: 'Shedule-event скрипты', link: '/examples_shedule_event.html' }
+                  ]
                }
-            ],
+            ]
          },
       ],
       sidebar: "auto",
@@ -42,6 +48,9 @@ module.exports = {
       logo: '/logo.png'
    }
 };
+
+
+
 
 
 
