a209edbfc5ca6f2faed54de6c529855e2f505e9f
vvzvlad <vvzvlad@gmail.com>
2019-01-15 15:57:30 +0300

    update build script

--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
-npx gluepress build &&
-rm -rf ./dist/ ;
-mkdir ./dist ;
-mkdir ./dist/site ;
-mkdir ./dist/site/docs ;
-mv ./.vuepress/dist/* ./dist/site/docs &&
-cp logo.png ./dist/site/docs &&
-cp -R images ./dist/site/docs &&
-http-server ./dist/
+rm -rf ./dist
+rm -rf ./.vuepress/dist
+#set -e
+npx gluepress build
+
+if [ $? -eq 0 ]
+then
+  mkdir ./dist
+  mkdir ./dist/site
+  mkdir ./dist/site/docs
+  mv ./.vuepress/dist/* ./dist/site/docs
+  cp logo.png ./dist/site/docs
+  cp -R images ./dist/site/docs
+  http-server ./dist/
+else
+  rm -rf ./dist
+  rm -rf ./.vuepress/dist
+fi
+
+
