72b4ffc8b6413fd8160434b4bd4679264ef4e7da
vvzvlad <vvzvlad@gmail.com>
2019-01-28 02:02:32 +0300

    update build file

--- build.sh
+++ build.sh
@@ -7,11 +7,10 @@ npx gluepress build
 if [ $? -eq 0 ]
 then
   mkdir ./dist
-  mkdir ./dist/site
-  mkdir ./dist/site/docs
-  mv ./.vuepress/dist/* ./dist/site/docs
-  cp logo.png ./dist/site/docs
-  cp -R images ./dist/site/docs
+  mkdir ./dist/docs
+  mv ./.vuepress/dist/* ./dist/docs
+  cp logo.png ./dist/docs
+  cp -R images ./dist/docs
   http-server ./dist/
 else
   rm -rf ./dist
