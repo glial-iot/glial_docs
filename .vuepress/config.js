9b7edd773b2a442f9fd16ed610ba7d676c7fb23e
vvzvlad <vvzvlad@gmail.com>
2019-04-04 04:55:28 +0300

    Revert "update structure"
    This reverts commit e1df3d36eed037a5cafee958ae0cddf7fe5e3c31.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,36 +4,34 @@ module.exports = {
    themeConfig: {
       nav: [
          {
-            text: 'Tutorials', items: [
-               { text: 'Запуск системы', link: '/start.html' }
-            ],
-         },
-         {
-            text: 'Howto guides', items: [
-               { text: 'Запуск системы', link: '/start.html' }
-            ],
-         },
-         {
-            text: 'Explanation', items: [
+            text: 'Основная информация', items: [
                { text: 'Что такое GLUE', link: '/what_glue.html' },
-               { text: 'Общая шина', link: '/bus.html' },
-               { text: 'Система логов', link: '/logs.html' },
+               { text: 'Запуск системы', link: '/start.html' },
+               { text: 'Панель управления', link: '/panel.html' },
                { text: 'Скрипты и драйвера', link: '/scripts_and_drivers.html' },
-               { text: 'Внутренности системы', link: '/inside.html' },
-               { text: 'Инструменты тестирования', link: '/tests.html' }
-            ],
+               { text: 'Система логов', link: '/logs.html' },
+               { text: 'Общая шина', link: '/bus.html' },
+               { text: 'Язык Lua', link: '/lua.html' }
+            ]
          },
          {
-            text: 'Reference', items: [
-               { text: 'История изменений', link: '/changelog.html' },
+            text: 'Документация', items: [
                { text: 'Панель управления', link: '/panel.html' },
                { text: 'Руководство разработчика', link: '/developers.html' },
-               { text: 'Язык Lua', link: '/lua.html' },
-               {
-                  text: 'Примеры'
-               }
-            ],
+               { text: 'Внутренности системы', link: '/inside.html' },
+               { text: 'Инструменты тестирования', link: '/tests.html' },
+               { text: 'История изменений', link: '/changelog.html' }
+            ]
          },
+         {
+            text: 'Примеры', items: [
+               { text: 'Драйвера', link: '/examples_driver.html' },
+               { text: 'Bus-event скрипты', link: '/examples_bus_event.html' },
+               { text: 'Web-event скрипты', link: '/examples_web_event.html' },
+               { text: 'Timer-event скрипты', link: '/examples_timer_event.html' },
+               { text: 'Shedule-event скрипты', link: '/examples_shedule_event.html' }
+            ]
+         }
       ],
       sidebar: "auto",
       sidebarDepth: 3,
